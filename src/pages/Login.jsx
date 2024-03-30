ef6b2781b65baae43c9a47b9f560a348a8a71bac
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-03-30 23:51:21 +0500

    Homewor is almost  done!

---
+++ src/pages/Login.jsx
@@ -0,0 +1,26 @@
+
+import Button from '@mui/material/Button';
+import { Box, TextField } from "@mui/material";
+function Login() {
+  return (
+    <>
+      <div className=" h-screen px-4 pt-10">
+        <div className=" container mx-auto block w-[500px] bg-slate-50 shadow-sm shadow-current text-center p-4 form column border-2">
+        <h1 className="text-4xl font-semibold mb-4">Login Page</h1>
+        <Box  component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 2, width: '25rem'},
+      }}
+      noValidate
+      autoComplete="off">
+        <TextField className="w-96" type="text" id="outlined-basic" label="User name" variant="outlined" /> 
+        <TextField className="w-96" type="password" id="outlined-basic" label="Password" variant="outlined" />
+        <Button className="w-56"  variant="contained">Send</Button>
+        </Box>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,29 @@
+import Button from '@mui/material/Button';
+import { Box, Link, TextField } from "@mui/material";
+function Register() {
+  return (
+    
+    <>
+      <div className=" h-screen px-4 pt-10">
+        <div className=" container mx-auto block w-[500px] bg-slate-50 shadow-sm shadow-current text-center p-4 form column border-2">
+        <h1 className="text-4xl font-semibold mb-4">Register Page</h1>
+        <Box  component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 2, width: '25rem'},
+      }}
+      noValidate
+      autoComplete="off">
+        <TextField className="w-96" type="text" id="outlined-basic" label="User name" variant="outlined" /> 
+        <TextField className="w-96" type="email"  id="outlined-basic" label="Email" variant="outlined" /> 
+        <TextField className="w-96" type="password" id="outlined-basic" label="Password" variant="outlined" />
+        <TextField className="w-96" type="password" id="outlined-basic" label="Repassword" variant="outlined" />
+        <Button className="w-56"  variant="contained">Send</Button>
+        </Box>
+        <span className='mt-2'>Already a member?</span> <Link to ="./Login.jsx">Login</Link>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Register;
